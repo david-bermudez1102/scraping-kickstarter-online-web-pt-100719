8035209841ad950aec3fc32d9047f6b2a3e58607
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-21 15:43:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,4 +5,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = nokogiri::HTML(html)
   projects = {}
+
+  kickstarter.each { |post|
+    projects.[post.css("h2.bbcard_name strong a").text] = "Hello"
+  }
 end
